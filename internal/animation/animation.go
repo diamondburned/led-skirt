c70c0acc347c9fbd4ad5686a6ddfe3af380b6a12
diamondburned <diamond@arikawa-hi.me>
2023-10-24 03:03:33 -0700

    Remove unused functions

--- internal/animation/animation.go
+++ internal/animation/animation.go
@@ -25,19 +25,3 @@ func CalculateDurationInU16(d, max Milliseconds) uint16 {
 // AnimationFunc is a function that accepts a time from Tstart to Tend and
 // returns the color of the LED at that time.
 type AnimationFunc func(d, max Milliseconds) colors.RGB
-
-// NewLinearInterpolator returns an AnimationFunc that interpolates between
-// the given colors using a linear function.
-func NewLinearInterpolator(c1, c2 colors.RGB) AnimationFunc {
-	return func(d, max Milliseconds) colors.RGB {
-		return linterpColor(c1, c2, CalculateDurationInU16(d, max))
-	}
-}
-
-func linterpColor(c1, c2 colors.RGB, scale uint16) colors.RGB {
-	return colors.RGB{
-		R: uint8(((uint32(c2.R) - uint32(c1.R)) * uint32(scale) / 0xFFFF) + uint32(c1.R)),
-		G: uint8(((uint32(c2.G) - uint32(c1.G)) * uint32(scale) / 0xFFFF) + uint32(c1.G)),
-		B: uint8(((uint32(c2.B) - uint32(c1.B)) * uint32(scale) / 0xFFFF) + uint32(c1.B)),
-	}
-}

5735346751abcc5e80c8213254c7b25fa837c870
diamondburned <diamond@arikawa-hi.me>
2023-11-02 04:55:38 -0700

    Switch code to new LED strip

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 	"tinygo.org/x/drivers/ws2812"
 )
 
-const brightness = 0.25 // operate at 25% brightness
+const utilization = 1   // utilize 100% of the LEDs
+const brightness = 0.15 // operate at 25% brightness
 const frameRate = 30    // operate at 30 FPS
-const numLEDs = 30      // 30 LEDs in the skirt
+const numLEDs = 60      // 60 LEDs in the skirt
 
 func main() {
 	// machine.LED.Configure(machine.PinConfig{Mode: machine.PinOutput})
@@ -22,16 +23,26 @@ func main() {
 	// Convert the animation duration to milliseconds for performance.
 	animationDurationMs := animation.DurationToMs(animationDuration)
 
+	var skipEvery int
+	if utilization < 1 {
+		skipEvery = int(1 / utilization)
+	}
+
 	start := time.Now()
 	for t := range time.Tick(time.Second / frameRate) {
 		ms := animation.DurationToMs(t.Sub(start)) % animationDurationMs
 		color := animationFunc(ms, animationDurationMs)
-		drawColor(ledStrip, color)
+		drawColor(ledStrip, color, skipEvery)
 	}
 }
 
-func drawColor(led ws2812.Device, c colors.RGB) {
+func drawColor(led ws2812.Device, c colors.RGB, skipEvery int) {
 	for i := 0; i < numLEDs; i++ {
+		c := c
+		if skipEvery != 0 && i%skipEvery == 0 {
+			c = colors.RGB{}
+		}
+
 		led.WriteByte(c.G) // WS2812 LEDs are GRB
 		led.WriteByte(c.R)
 		led.WriteByte(c.B)
